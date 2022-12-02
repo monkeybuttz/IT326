97b77b93d1e5f0fdbb41d7c53a31cbe1a045cee5
Gyonan <g2yonan@gmail.com>
2022-12-02 17:49:05 -0600

    f

--- backend/src/test/java/MessageTest.java
+++ backend/src/test/java/MessageTest.java
@@ -1,4 +1,4 @@
-package backend.src.test;
+
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
--- backend/src/test/java/UserTesting.java
+++ backend/src/test/java/UserTesting.java
@@ -1,6 +1,8 @@
 
 import java.sql.SQLException;
 import com.jdbc.model.Owner;
+import com.jdbc.model.User;
+
 import org.junit.jupiter.api.Test;
 
 
@@ -8,7 +10,9 @@ public class UserTesting {
 
     @Test
     public void testingCreateUserAccount() throws SQLException {
-        Owner owner1 = new Owner(65, "Greg Yonan", "gyonan", "gyonan@ilstu.edu", "Password1234!", 84790, "enter this", false);
+        User owner1 = new Owner(65, "Greg Yonan", "gyonan", "gyonan@ilstu.edu", "Password1234!", 84790, "enter this", false);
+        System.out.println(owner1.getEmail());
         owner1.createAccount();
     }
+
 }
