4e51ad79d87282a54a221d27065edad86b31ace2
Omar Baloch <100452153+obaloch1997@users.noreply.github.com>
2022-12-05 03:24:20 -0600

    update to DownloadPetProfile.java

--- backend/src/main/java/com/jdbc/main/DownloadPetProfile.java
+++ backend/src/main/java/com/jdbc/main/DownloadPetProfile.java
@@ -3,53 +3,59 @@ package com.jdbc.main;
 import java.io.*;
 import java.sql.*;
 
+import com.jdbc.util.JDBCConnection;
 /*
  * Program to get pet profile and store into
  * a csv file.
  */
 public class DownloadPetProfile {
-
+ 
     public static void main(String[] args) {
-
-        downloadPetProfile(1);
+        
+    	downloadPetProfile(1);
     }
-
-    public static void downloadPetProfile(int petId) {
-        String url = "jdbc:mysql://localhost/petcare";
-        String password = "ParkSideRoad161997";
-        String username = "root";
-
+    
+    public static String downloadPetProfile(int petId)
+    {
+        String linne = " ";
         String csvFilePath = "petProfile.csv";
-
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+         
+        try {
+        	
+        	Connection con = JDBCConnection.getConnection();
             String sql = "SELECT * FROM pet WHERE petID = " + petId;
-
-            Statement statement = connection.createStatement();
-
+             
+            Statement statement = con.createStatement();
+             
             ResultSet result = statement.executeQuery(sql);
-
+             
             BufferedWriter fileWriter = new BufferedWriter(new FileWriter(csvFilePath));
-
-            // write header line containing column names
+             
+            // write header line containing column names       
             fileWriter.write("petID,ownerID,name, breed, notes");
-
+//            static String line = " ";
+             
             while (result.next()) {
                 int petId2 = result.getInt("petID");
                 int ownerId = result.getInt("ownerID");
                 String name = result.getString("name");
                 String breed = result.getString("breed");
                 String notes = result.getString("notes");
-
-                String line = String.format("\"%d\",%d, %s, %s, %s",
+                 
+                String line2 = String.format("%d,%d, %s, %s, %s",
                         petId2, ownerId, name, breed, notes);
-
+                System.out.println(line2);
+                linne = linne + line2;
+                
+                 
                 fileWriter.newLine();
-                fileWriter.write(line);
+                fileWriter.write(line2);
+//                return line2;
             }
-
+             
             statement.close();
             fileWriter.close();
-
+             
         } catch (SQLException e) {
             System.out.println("Datababse error:");
             e.printStackTrace();
@@ -57,7 +63,10 @@ public class DownloadPetProfile {
             System.out.println("File IO error:");
             e.printStackTrace();
         }
-
+//    	return 0;
+//        System.out.println(line);
+//        return 0;
+        return linne;
     }
-
+ 
 }
